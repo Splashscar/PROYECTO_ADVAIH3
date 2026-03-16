1ce1bdec9693f6b7ddf9cdc4b87345fc678443a8
DavidAnzola05 <davidanzola014@gmail.com>
2026-03-16 10:31:39 -0500

    Crear la funcion de login en AI_CLIENT

--- ai_client.py
+++ ai_client.py
@@ -4,3 +4,11 @@ from google import genai
 from google.genai import types
 import requests
 
+# 1. Funcion de login
+
+def login_usuario():
+    print("--- login de usuario ---")
+    email = input("Email: ")
+    password = getpass.getpass("Contraseña: ")
+
+    url_login = "http://127.0.0.1:8000/api/auth/login/"
