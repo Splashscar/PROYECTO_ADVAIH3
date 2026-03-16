63de07e715ce777c6a2dcb3d546353793906550f
Splashscar <felipemendieta389@gmail.com>
2026-03-16 10:53:52 -0500

    correccion vistas y urls

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('auth/registro/', RegistroAPIView.as_view(), name='api_registro'),
     path('auth/login/', loginAPIView.as_view(), name='api_eventos'),
     path('eventos/', EventoAPIView.as_view(), name='api_eventos'),
-    path('eventos/<str:id>', EventoAPIView.as_view(), name='api_evento_detalle'),
+    path('eventos/<str:id>/', EventoAPIView.as_view(), name='api_evento_detalle'),
     path('perfil/foto/', PerfilImagenAPIView.as_view(), name='api_perfil_foto'),
 
 ]
