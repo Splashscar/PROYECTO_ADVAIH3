10863e6a59b305f33cb287b9ef560b37c5c2045f
Splashscar <felipemendieta389@gmail.com>
2026-03-16 11:15:30 -0500

    Commit con test casi terminado

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,9 +5,10 @@ from .views_perfil import PerfilImagenAPIView
 
 urlpatterns = [
     path('auth/registro/', RegistroAPIView.as_view(), name='api_registro'),
-    path('auth/login/', loginAPIView.as_view(), name='api_eventos'),
+    path('auth/login/', loginAPIView.as_view(), name='api_login'),
+    # Primero la ruta de la lista/creación
     path('eventos/', EventoAPIView.as_view(), name='api_eventos'),
+    # Luego la del detalle, y asegúrate de que termine en /
     path('eventos/<str:id>/', EventoAPIView.as_view(), name='api_evento_detalle'),
     path('perfil/foto/', PerfilImagenAPIView.as_view(), name='api_perfil_foto'),
-
 ]
