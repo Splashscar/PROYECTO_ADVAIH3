35ba04bde77c09e1ae533b0842399353e1fcda5e
DavidAnzola05 <davidanzola014@gmail.com>
2026-03-18 08:15:12 -0500

    Configurar envío de correos en Django tu settings.py

--- proyecto_advaih/settings.py
+++ proyecto_advaih/settings.py
@@ -125,3 +125,11 @@ cloudinary.config(
   api_key = "TU_API_KEY",
   api_secret = "TU_API_SECRET"
 )
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'davidanzolacs@gmail.com'
+EMAIL_HOST_PASSWORD = 'shoc ifdv idef kiwd'
